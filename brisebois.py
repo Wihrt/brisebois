65ba253326e46958c0b40ecc5c63e1f473be0b77
BuildTools <unconfigured@null.spigotmc.org>
2017-07-04 14:33:21 +0200

    Add logging utilities

--- brisebois.py
+++ brisebois.py
@@ -1,21 +1,43 @@
 #!/bin/env python
+from datetime import datetime
 from discord.ext.commands.bot import Bot
+from logging import getLogger, FileHandler, info, DEBUG, INFO
 from os import listdir
 from os.path import splitext
 from pymongo import MongoClient
 from sys import stderr
-from traceback import print_exception
+from traceback import print_tb
 import discord.ext.commands.errors as err
 
+# Logging
+discord_logger = getLogger('discord')
+# discord_logger.setLevel(DEBUG)
+discord_logger.setLevel(INFO)
+log = getLogger()
+# log.setLevel(DEBUG)
+log.setLevel(INFO)
+handler = FileHandler(filename='brisebois.log', encoding='utf-8', mode='w')
+log.addHandler(handler)
+
+
 # Create the bot
-bot = Bot(command_prefix="$",
-          pm_help=True)
+bot = Bot(command_prefix="$", pm_help=True)
 
 
 @bot.event
 async def on_ready():
-    """Message to be sent when the bot is ready to receive commands."""
-    print("Logged in as %s" % bot.user.name)
+    info('Logged in as:')
+    info('Username: ' + bot.user.name)
+    info('ID: ' + bot.user.id)
+    info('------')
+    if not hasattr(bot, 'uptime'):
+        bot.uptime = datetime.utcnow()
+
+
+@bot.event
+async def on_command(command, ctx):
+    info("Command : {0.command} called".format(ctx))
+    info("Message : {0.content}".format(ctx.message))
 
 
 @bot.event
@@ -27,23 +49,23 @@ async def on_command_error(error, ctx):
     elif isinstance(error, err.CommandOnCooldown):
         await bot.send_message(
             ctx.message.author,
-            "This command is on cooldown, retry after : %i seconds" %
-            error.retry_after)
-        await bot.delete_message(ctx.message)
+            "This command is on cooldown, retry after {} seconds".format(
+                error.retry_after))
+    elif isinstance(error, err.DisabledCommand):
+        await bot.send_message(
+            ctx.message.author,
+            "Sorry. This command is disabled and cannot be used.")
     elif isinstance(error, err.CommandNotFound):
         await bot.send_message(
             ctx.message.author,
-            "No command %s is found\nUse $help to see the list of commands" %
-            ctx.message.content)
+            "No command {} is found\n\
+Use $help to see the list of commands".format(ctx.message.content))
         await bot.delete_message(ctx.message)
-    elif isinstance(error, err.CheckFailure):
-        await bot.send_message(
-            ctx.message.author,
-            "You don't have the roles/permissions to use this command")
-    else:
-        print('Ignoring exception in command {}'.format(ctx.command),
+    elif isinstance(error, err.CommandInvokeError):
+        print('In {0.command.qualified_name}:'.format(ctx), file=stderr)
+        print_tb(error.original.__traceback__)
+        print('{0.__class__.__name__}: {0}'.format(error.original),
               file=stderr)
-        print_exception(type(error), error, error.__traceback__, file=stderr)
 
 
 @bot.event
